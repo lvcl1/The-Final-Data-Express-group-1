7255ca4d9a96668d35606a3ed622ef9fdaf8c3b4
joflores18 <100979201+joflores18@users.noreply.github.com>
2022-11-30 19:29:59 -0700

    Database and API

--- index.js
+++ index.js
@@ -3,13 +3,37 @@ const express = require('express'),
     path = require('path'),
     routes = require('./routes/routes'),
     bodyParser = require('body-parser');
+    const cookieParser = require('cookie-parser')
 
 const urlencodedParser = bodyParser.urlencoded({extended: true})
 const app = express();
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser('your passphrase here'))
+
+let questionData = [
+    {Question1: 'Do you prefer dogs or cats?', answer1: 0},
+    {Question2: 'Pineapple on pizza?', answer2: 0},
+    {Question3: 'Sub or dub for anime?' , answer3: 0}
+]
 
 app.get('/', routes.index);
+
+app.get('/setApi' , (req, res) => {
+    let setData = JSON.stringify(questionData)
+    res.cookie('questionData', setData, {maxAge: 999999999999})
+    res.send(`Set the cookie data: ${setData}`)
+})
+
+app.get('/getApi', (req, res) => {
+    if(req.cookies.questionData != undefined){
+        let getData = JSON.parse(req.cookies.questionData)
+        res.json(getData)
+    }
+    else{
+        res.send('There is no cookie here')
+    }
+})
 
 app.listen(3000);
--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,5 @@
+//Put mongoose here in routes where it will be used
+const mongoose = require('mongoose')
 //for the password salting
 const bcrypt = require("bcrypt")
 let password="pass"//will change later
@@ -6,8 +8,48 @@ console.log(salt)
 const hash=bcrypt.hashSync(password,salt)
 const { response } = require('express');
 
+//This statement is for mongoose 4 or earlier
+mongoose.Promise = global.Promise
+
+//Connect mongoose to the database
+// const connectionString = 'mongodb://localhost/data'
+const connectionString = 'mongodb+srv://root:Passroot1234@cluster0.i0jkmej.mongodb.net/test'
+
+mongoose.connect(connectionString, {
+    useUnifiedTopology: true
+})
+
+//Establish the connection with the database
+const db = mongoose.connection
+
+//Bind the connection to an errot event (called for each request)
+db.on('error', console.error.bind(console, 'DB Connection Error'))
+
+//Bind the connection to a function that will be called when db is opened(called once)
+db.once('open', callback => {})
+
+//Create a schema to define the fields, validation requirements and defaults
+let questionSchema = mongoose.Schema({
+    usename: String,
+    password: String,
+    email: String,
+    age: String,
+    answer1: String,
+    answer2: String,
+    answer3: String
+})
+
+// Create a model, Used for CRUD operations
+let Data = mongoose.model("People_Collection", questionSchema)
+
 exports.index = (req, res) => {
-    res.render('index', {
-        title: 'Login Page'
+    //query the table and find all the records
+    Data.find((err, data) => {
+        if(err) return console.error(err)
+
+        res.render('index', {
+            title: 'Data List',
+            data
+        })
     })
 }
