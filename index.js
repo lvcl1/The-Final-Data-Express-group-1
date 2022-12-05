47aa4ab6274de217c220981a9c6b9d1f6f4537af
joflores18 <100979201+joflores18@users.noreply.github.com>
2022-12-04 19:11:12 -0700

    API Done

--- index.js
+++ index.js
@@ -9,36 +9,18 @@ const urlencodedParser = bodyParser.urlencoded({extended: true})
 const app = express();
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'pubic')));
 app.use(cookieParser('your passphrase here'))
 
-let questionData = [
-    {Question1: 'Do you prefer dogs or cats?', answer1: 0},
-    {Question2: 'Pineapple on pizza?', answer2: 0},
-    {Question3: 'Sub or dub for anime?' , answer3: 0}
-]
-app.set('views', path.join(__dirname, '/views'));
-app.use(express.static(path.join(__dirname, '/public')));
+
 
 app.get('/', routes.index);
 app.get('/createuser', routes.createuser);
 app.post("/submitteduser", urlencodedParser, routes.submittedUser);
+app.get('/setApi', routes.setApi)
+app.get('/api', routes.getApi)
 
-app.get('/setApi' , (req, res) => {
-    let setData = JSON.stringify(questionData)
-    res.cookie('questionData', setData, {maxAge: 999999999999})
-    res.send(`Set the cookie data: ${setData}`)
-})
 
-app.get('/getApi', (req, res) => {
-    if(req.cookies.questionData != undefined){
-        let getData = JSON.parse(req.cookies.questionData)
-        res.json(getData)
-    }
-    else{
-        res.send('There is no cookie here')
-    }
-})
 app.post('/loggedin', urlencodedParser, routes.login);
 
 app.listen(3000);
--- routes/routes.js
+++ routes/routes.js
@@ -47,15 +47,16 @@ let counterSchema = mongoose.Schema({
 
 // Create a model, Used for CRUD operations
 let Data = mongoose.model("People_Collection", questionSchema)
+let Counter = mongoose.model("Counter", counterSchema)
 
 exports.index = (req, res) => {
     //query the table and find all the records
-    Data.find((err, data) => {
+    Counter.find((err, questions) => {
         if(err) return console.error(err)
 
         res.render('index', {
             title: 'Data List',
-            data
+            questions
         })
     })
 }
@@ -74,32 +75,96 @@ exports.submittedUser=(req,res)=>{
         answer2: req.body.answer2,
         answer3: req.body.answer3
     })
-
     user.save((error, user) => {
         if (error) res.send(error);
         console.log(`${req.body.username} added`)
     })
 
+    Data.find((err, users) => {
+        if(err) res.send(err)
+
+        var answerOne = 0
+        var answerTwo = 0
+        var answerThree = 0
+        var answerFour = 0
+        var answerFive = 0
+        var answerSix = 0
+        users.forEach(function(user){
+            let useranswer1 = user.answer1
+            let useranswer2 = user.answer2
+            let useranswer3 = user.answer3
+
+            if(useranswer1 == 'dogs'){
+                answerOne = answerOne + 1
+            }
+            else if(useranswer1 == 'cats'){
+                answerTwo = answerTwo + 1
+            }
+
+            if(useranswer2 == "yes"){
+                answerThree = answerThree + 1
+            }
+            else if(useranswer2 == "no"){
+                answerFour = answerFour + 1
+            }
+
+            if(useranswer3 == "sub"){
+                answerFive = answerFive + 1
+            }
+            else if(useranswer3 == "dub"){
+                answerSix = answerSix + 1
+            }
+        })
+        Counter.findById("638d2d6ddb1c1932d476f94e", (err, user) => {
+            if(err) res.send(err)
+            user.answerOne = answerOne
+            user.answerTwo = answerTwo
+            user.save((err, user) => {
+                if(err) res.send(err)
+            })
+        })
+        Counter.findById("638d2d6ddb1c1932d476f94f", (err, user) => {
+            if(err) res.send(err)
+            user.answerOne = answerThree
+            user.answerTwo = answerFour
+            user.save((err, user) => {
+                if(err) res.send(err)
+            })
+        })
+        Counter.findById("638d2d6ddb1c1932d476f950", (err, user) => {
+            if(err) res.send(err)
+            user.answerOne = answerFive
+            user.answerTwo = answerSix
+            user.save((err, user) => {
+                if(err) res.send(err)
+            })
+        })
+    })
+
     res.redirect('/');
-    // let userRecord=`
-    // name: ${user.name}
-    // password: ${user.password}
-    // email: ${user.email}
-    // age: ${user.age}
-    // answer1: ${user.answer1}
-    // answer2: ${user.answer2}
-    // answer3: ${user.answer3}
-    // `;
-    // fs.appendFile("names.txt",personRecord,err=>{
-    //     if (err) throw err;
-    //     else console.log("saved");
-    // });
     console.log(user)
-    // res.render("submitted",{
-    //     'title':"submitted",
-    //     "config":config,
-    //     "person":user
-    // })
+}
+
+exports.setApi = (req, res) => {
+    Counter.find((err, questions) => {
+        if(err) res.send(err)
+        let setData = JSON.stringify(questions)
+        res.cookie('questions', setData, {maxAge: 999999999999})
+        res.send(`Set the cookie data: ${setData}`)
+    })
+}
+
+exports.getApi = (req, res) => {
+    Counter.find((err, questions) => {
+        if(err) res.send(err)
+        if(req.cookies.questions != undefined){
+            let getData = JSON.parse(req.cookies.questions)
+            res.json(getData)
+        }
+        else{
+            res.send('There is no cookie here')
+        }
+    })
 }
 
 exports.login = (req, res) => {
@@ -127,9 +192,7 @@ exports.login = (req, res) => {
                 
             }
         })
-
         console.log(check)
-
         if(check == true) {
             res.render('loggedin', {
                 title: 'Welcome',
@@ -141,7 +204,5 @@ exports.login = (req, res) => {
                 errrorLog: 'Invalid Credentials'
             })
         }
-
     })
-    
 }
